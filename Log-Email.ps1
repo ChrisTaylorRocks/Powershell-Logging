8cc8a68dc40b19b6d83927b32cb07864ede673d7
Chris Taylor <chris@christaylor.rocks>
2016-07-16 11:17:57 -0600

    Initial upload

---
+++ Log-Email.ps1
@@ -0,0 +1,56 @@
+﻿Function Log-Email{
+  <#
+  .SYNOPSIS
+    Emails log file to list of recipients
+
+  .DESCRIPTION
+    Emails the contents of the specified log file to a list of recipients
+  
+  .PARAMETER LogPath
+    Mandatory. Full path of the log file you want to email. Example: C:\Windows\Temp\Test_Script.log
+  
+  .PARAMETER EmailFrom
+    Mandatory. The email addresses of who you want to send the email from. Example: "admin@9to5IT.com"
+
+  .PARAMETER EmailTo
+    Mandatory. The email addresses of where to send the email to. Seperate multiple emails by ",". Example: "admin@9to5IT.com, test@test.com"
+  
+  .PARAMETER EmailSubject
+    Mandatory. The subject of the email you want to send. Example: "Cool Script - [" + (Get-Date).ToShortDateString() + "]"
+
+  .INPUTS
+    Parameters above
+
+  .OUTPUTS
+    Email sent to the list of addresses specified
+
+  .NOTES
+    Version:        1.0
+    Author:         Luca Sturlese
+    Creation Date:  05.10.12
+    Purpose/Change: Initial function development
+
+  .EXAMPLE
+    Log-Email -LogPath "C:\Windows\Temp\Test_Script.log" -EmailFrom "admin@9to5IT.com" -EmailTo "admin@9to5IT.com, test@test.com" -EmailSubject "Cool Script - [" + (Get-Date).ToShortDateString() + "]"
+  #>
+  
+  [CmdletBinding()]
+  
+  Param ([Parameter(Mandatory=$true)][string]$sLogPath, [Parameter(Mandatory=$true)][string]$EmailFrom, [Parameter(Mandatory=$true)][string]$EmailTo, [Parameter(Mandatory=$true)][string]$EmailSubject)
+  
+  Process{
+    Try{
+      $sBody = (Get-Content $sLogPath | out-string)
+      
+      #Create SMTP object and send email
+      $sSmtpServer = "server1.wefilterspam.com"
+      $oSmtp = new-object Net.Mail.SmtpClient($sSmtpServer)
+      $oSmtp.Send($EmailFrom, $EmailTo, $EmailSubject, $sBody)
+      Exit 0
+    }
+    
+    Catch{
+      Exit 1
+    } 
+  }
+}
