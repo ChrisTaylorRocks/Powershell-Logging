e4171a4918886dead5c58cf123b51e261b99b562
Chris Taylor <chris@christaylor.rocks>
2016-07-16 11:10:21 -0600

    initial upload

---
+++ Log-Start.ps1
@@ -0,0 +1,86 @@
+﻿Function Log-Start{
+  <#
+  .SYNOPSIS
+    Creates log file
+
+  .DESCRIPTION
+    Creates log file with path and name that is passed. Checks if log file exists, and if it does deletes it and creates a new one.
+    Once created, writes initial logging data
+
+  .PARAMETER LogPath
+    Mandatory. Path of where log is to be created. Example: C:\Windows\Temp
+
+  .PARAMETER LogName
+    Mandatory. Name of log file to be created. Example: Test_Script.log
+      
+  .PARAMETER ScriptVersion
+    Mandatory. Version of the running script which will be written in the log. Example: 1.5
+
+  .INPUTS
+    Parameters above
+
+  .OUTPUTS
+    Log file created
+
+  .NOTES
+    Version:        1.0
+    Author:         Luca Sturlese
+    Creation Date:  10/05/12
+    Purpose/Change: Initial function development
+
+    Version:        1.1
+    Author:         Luca Sturlese
+    Creation Date:  19/05/12
+    Purpose/Change: Added debug mode support
+
+    Version:        1.2
+    Author:         Chris Taylor
+    Creation Date:  7/17/2015
+    Purpose/Change: Added directory creation if not present.
+
+
+  .EXAMPLE
+    Log-Start -LogPath "C:\Windows\Temp" -LogName "Test_Script.log" -ScriptVersion "1.5"
+  #>
+    
+  [CmdletBinding()]
+  
+  Param ([Parameter(Mandatory=$true)][string]$sLogPath, [Parameter(Mandatory=$true)][string]$LogName, [Parameter(Mandatory=$true)][string]$ScriptVersion, [Parameter(Mandatory=$false)][switch]$Append)
+  
+  Process{
+    $sFullPath = $sLogPath + "\" + $LogName
+    #Check if file exists and delete if it does
+    If((Test-Path -Path $sFullPath) -and $Append -ne $true){
+      Remove-Item -Path $sFullPath -Force
+    }
+
+    #Check if folder exists if not create    
+    If((Test-Path -PathType Container -Path $sLogPath) -eq $False){
+      New-Item -ItemType Directory -Force -Path $sLogPath
+    }
+
+    #Create file and start logging
+    If($(Test-Path -Path $sFullPath) -ne $true) {
+        New-Item -Path $sLogPath -Value $LogName -ItemType File
+    }
+
+    Add-Content -Path $sFullPath -Value "***************************************************************************************************"
+    Add-Content -Path $sFullPath -Value "Started processing at [$([DateTime]::Now)]."
+    Add-Content -Path $sFullPath -Value "***************************************************************************************************"
+    Add-Content -Path $sFullPath -Value ""
+    Add-Content -Path $sFullPath -Value "Running script version [$ScriptVersion]."
+    Add-Content -Path $sFullPath -Value ""
+    Add-Content -Path $sFullPath -Value "***************************************************************************************************"
+    Add-Content -Path $sFullPath -Value ""
+  
+    #Write to screen for debug mode
+    Write-Debug "***************************************************************************************************"
+    Write-Debug "Started processing at [$([DateTime]::Now)]."
+    Write-Debug "***************************************************************************************************"
+    Write-Debug ""
+    Write-Debug "Running script version [$ScriptVersion]."
+    Write-Debug ""
+    Write-Debug "***************************************************************************************************"
+    Write-Debug ""
+  }
+}
