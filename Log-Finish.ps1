ebc930b0b5498ec85089a5bd76cb006d3744934f
Chris Taylor <chris@christaylor.rocks>
2016-07-16 11:16:13 -0600

    Initial upload

---
+++ Log-Finish.ps1
@@ -0,0 +1,65 @@
+﻿Function Log-Finish{
+  <#
+  .SYNOPSIS
+    Write closing logging data & exit
+
+  .DESCRIPTION
+    Writes finishing logging data to specified log and then exits the calling script
+  
+  .PARAMETER LogPath
+    Mandatory. Full path of the log file you want to write finishing data to. Example: C:\Windows\Temp\Test_Script.log
+
+  .PARAMETER NoExit
+    Optional. If this is set to True, then the function will not exit the calling script, so that further execution can occur
+  
+  .INPUTS
+    Parameters above
+
+  .OUTPUTS
+    None
+
+  .NOTES
+    Version:        1.0
+    Author:         Luca Sturlese
+    Creation Date:  10/05/12
+    Purpose/Change: Initial function development
+    
+    Version:        1.1
+    Author:         Luca Sturlese
+    Creation Date:  19/05/12
+    Purpose/Change: Added debug mode support
+  
+    Version:        1.2
+    Author:         Luca Sturlese
+    Creation Date:  01/08/12
+    Purpose/Change: Added option to not exit calling script if required (via optional parameter)
+
+  .EXAMPLE
+    Log-Finish -LogPath "C:\Windows\Temp\Test_Script.log"
+
+.EXAMPLE
+    Log-Finish -LogPath "C:\Windows\Temp\Test_Script.log" -NoExit $True
+  #>
+  
+  [CmdletBinding()]
+  
+  Param ([Parameter(Mandatory=$true)][string]$sLogPath, [Parameter(Mandatory=$false)][string]$NoExit)
+  
+  Process{
+    Add-Content -Path $sLogPath -Value ""
+    Add-Content -Path $sLogPath -Value "***************************************************************************************************"
+    Add-Content -Path $sLogPath -Value "Finished processing at [$([DateTime]::Now)]."
+    Add-Content -Path $sLogPath -Value "***************************************************************************************************"
+  
+    #Write to screen for debug mode
+    Write-Debug ""
+    Write-Debug "***************************************************************************************************"
+    Write-Debug "Finished processing at [$([DateTime]::Now)]."
+    Write-Debug "***************************************************************************************************"
+  
+    #Exit calling script if NoExit has not been specified or is set to False
+    If(!($NoExit) -or ($NoExit -eq $False)){
+      Exit
+    }    
+  }
+}
