fc576764baab7dce24f9e55763f64c4e64eec7c8
Chris Taylor <chris@christaylor.rocks>
2016-07-16 11:14:30 -0600

    Initial upload

---
+++ Log-Error.ps1
@@ -0,0 +1,55 @@
+﻿Function Log-Error{
+  <#
+  .SYNOPSIS
+    Writes an error to a log file
+
+  .DESCRIPTION
+    Writes the passed error to a new line at the end of the specified log file
+  
+  .PARAMETER LogPath
+    Mandatory. Full path of the log file you want to write to. Example: C:\Windows\Temp\Test_Script.log
+  
+  .PARAMETER ErrorDesc
+    Mandatory. The description of the error you want to pass (use $_.Exception)
+  
+  .PARAMETER ExitGracefully
+    Mandatory. Boolean. If set to True, runs Log-Finish and then exits script
+
+  .INPUTS
+    Parameters above
+
+  .OUTPUTS
+    None
+
+  .NOTES
+    Version:        1.0
+    Author:         Luca Sturlese
+    Creation Date:  10/05/12
+    Purpose/Change: Initial function development
+    
+    Version:        1.1
+    Author:         Luca Sturlese
+    Creation Date:  19/05/12
+    Purpose/Change: Added debug mode support. Added -ExitGracefully parameter functionality
+
+  .EXAMPLE
+    Log-Error -LogPath "C:\Windows\Temp\Test_Script.log" -ErrorDesc $_.Exception -ExitGracefully $True
+  #>
+  
+  [CmdletBinding()]
+  
+  Param ([Parameter(Mandatory=$true)][string]$sLogPath, [Parameter(Mandatory=$true)][string]$ErrorDesc, [Parameter(Mandatory=$true)][boolean]$ExitGracefully)
+  
+  Process{
+    Add-Content -Path $sLogPath -Value "Error: An error has occurred [$ErrorDesc]."
+  
+    #Write to screen for debug mode
+    Write-Debug "Error: An error has occurred [$ErrorDesc]."
+    
+    #If $ExitGracefully = True then run Log-Finish and exit script
+    If ($ExitGracefully -eq $True){
+      Log-Finish -LogPath $sLogPath
+      Breaåk
+    }
+  }
+}
