dc5ed459577c2eee90fc65ab1228e4fa68510d98
Chris Taylor <chris@christaylor.rocks>
2016-07-16 12:36:02 -0600

    Updated URL's

--- Powershell-Logging.ps1
+++ Powershell-Logging.ps1
@@ -1,7 +1,8 @@
 ﻿    #This will load all Powershell-Logging functions
     #https://github.com/ChrisTaylorRocks/Powershell-Logging
-    (new-object Net.WebClient).DownloadString("https://raw.githubusercontent.com/ChrisTaylorRocks/Powershell-Logging/master/Log-Start.ps1") | iex
-    (new-object Net.WebClient).DownloadString("https://raw.githubusercontent.com/ChrisTaylorRocks/Powershell-Logging/master/Log-Write.ps1") | iex
-    (new-object Net.WebClient).DownloadString("https://raw.githubusercontent.com/ChrisTaylorRocks/Powershell-Logging/master/Log-Error.ps1") | iex
-    (new-object Net.WebClient).DownloadString("https://raw.githubusercontent.com/ChrisTaylorRocks/Powershell-Logging/master/Log-Finish.ps1") | iex
     (new-object Net.WebClient).DownloadString("https://raw.githubusercontent.com/ChrisTaylorRocks/Powershell-Logging/master/Log-Email.ps1") | iex
+    (new-object Net.WebClient).DownloadString("https://raw.githubusercontent.com/ChrisTaylorRocks/Powershell-Logging/master/Start-Log.ps1") | iex
+    (new-object Net.WebClient).DownloadString("https://raw.githubusercontent.com/ChrisTaylorRocks/Powershell-Logging/master/Stop-Log.ps1") | iex
+    (new-object Net.WebClient).DownloadString("https://raw.githubusercontent.com/ChrisTaylorRocks/Powershell-Logging/master/Write-LogError.ps1") | iex
+    (new-object Net.WebClient).DownloadString("https://raw.githubusercontent.com/ChrisTaylorRocks/Powershell-Logging/master/Write-LogInfo.ps1") | iex
+    (new-object Net.WebClient).DownloadString("https://raw.githubusercontent.com/ChrisTaylorRocks/Powershell-Logging/master/Write-LogWarning.ps1") | iex
