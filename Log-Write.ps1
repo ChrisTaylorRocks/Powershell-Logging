044effebf60f9c26e7e58e4d9c5ccd2594f94ad7
Chris Taylor <chris@christaylor.rocks>
2016-07-16 11:12:21 -0600

    Initial upload

---
+++ Log-Write.ps1
@@ -0,0 +1,46 @@
+﻿Function Log-Write{
+  <#
+  .SYNOPSIS
+    Writes to a log file
+
+  .DESCRIPTION
+    Appends a new line to the end of the specified log file
+  
+  .PARAMETER LogPath
+    Mandatory. Full path of the log file you want to write to. Example: C:\Windows\Temp\Test_Script.log
+  
+  .PARAMETER LineValue
+    Mandatory. The string that you want to write to the log
+      
+  .INPUTS
+    Parameters above
+
+  .OUTPUTS
+    None
+
+  .NOTES
+    Version:        1.0
+    Author:         Luca Sturlese
+    Creation Date:  10/05/12
+    Purpose/Change: Initial function development
+  
+    Version:        1.1
+    Author:         Luca Sturlese
+    Creation Date:  19/05/12
+    Purpose/Change: Added debug mode support
+
+  .EXAMPLE
+    Log-Write -LogPath "C:\Windows\Temp\Test_Script.log" -LineValue "This is a new line which I am appending to the end of the log file."
+  #>
+  
+  [CmdletBinding()]
+  
+  Param ([Parameter(Mandatory=$true)][string]$sLogPath, [Parameter(Mandatory=$true)][string]$LineValue)
+  
+  Process{
+    Add-Content -Path $sLogPath -Value $LineValue
+  
+    #Write to screen for debug mode
+    Write-Debug $LineValue
+  }
+}
