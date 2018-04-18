abf808d16795c4313409b8f1951fc0131f504bf4
Chris Taylor <chris@christaylor.rocks>
2018-04-18 15:28:23 -0600

    Update Powershell-Logging.ps1

--- Powershell-Logging.ps1
+++ Powershell-Logging.ps1
@@ -1,4 +1,4 @@
-﻿    #This will load all Powershell-Logging functions
+    #This will load all Powershell-Logging functions
     #https://github.com/ChrisTaylorRocks/Powershell-Logging
     (new-object Net.WebClient).DownloadString("https://raw.githubusercontent.com/ChrisTaylorRocks/Powershell-Logging/master/Log-Email.ps1") | iex
     (new-object Net.WebClient).DownloadString("https://raw.githubusercontent.com/ChrisTaylorRocks/Powershell-Logging/master/Start-Log.ps1") | iex
@@ -6,3 +6,4 @@
     (new-object Net.WebClient).DownloadString("https://raw.githubusercontent.com/ChrisTaylorRocks/Powershell-Logging/master/Write-LogError.ps1") | iex
     (new-object Net.WebClient).DownloadString("https://raw.githubusercontent.com/ChrisTaylorRocks/Powershell-Logging/master/Write-LogInfo.ps1") | iex
     (new-object Net.WebClient).DownloadString("https://raw.githubusercontent.com/ChrisTaylorRocks/Powershell-Logging/master/Write-LogWarning.ps1") | iex
+    (new-object Net.WebClient).DownloadString("https://raw.githubusercontent.com/ChrisTaylorRocks/Powershell-Logging/master/Set-LogSettings.ps1") | iex
